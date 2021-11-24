272bd680a2e559f98b427d3886702cfedcebdd77
Alex Stich <leftyfl1p@me.com>
2021-11-24 13:33:15 -0800

    Wtf javascript

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,11 +4,11 @@ export function readInt24LE(buf: Buffer, offset: number): number {
         throw new Error('Buffer too small');
     }
 
-    let ret: number = tmp[0] & 0x0000ff;
-    ret |= (tmp[1] << 8) & 0x00ff00;
-    ret |= (tmp[2] << 16) & 0xff0000;
-    if ((tmp[2] & 0x80) == 0x80) {
-        ret |= 0xff << 24;
+    let ret = (tmp[0] & 0x0000ff) >>> 0;
+    ret = (ret | ((tmp[1] << 8) & 0x00ff00)) >>> 0;
+    ret = (ret | ((tmp[2] << 16) & 0xff0000)) >>> 0;
+    if ((tmp[2] & 0x80) >>> 0 == 0x80) {
+        ret = (ret | (0xff << 24)) >>> 0;
     }
 
     return ret;
@@ -20,11 +20,11 @@ export function readInt24BE(buf: Buffer, offset: number): number {
         throw new Error('Buffer too small');
     }
 
-    let ret = tmp[2] & 0x0000ff;
-    ret |= (tmp[1] << 8) & 0x00ff00;
-    ret |= (tmp[0] << 16) & 0xff0000;
-    if ((tmp[0] & 0x80) == 0x80) {
-        ret |= 0xff << 24;
+    let ret = (tmp[2] & 0x0000ff) >>> 0;
+    ret = (ret | ((tmp[1] << 8) & 0x00ff00)) >>> 0;
+    ret = (ret | ((tmp[0] << 16) & 0xff0000)) >>> 0;
+    if ((tmp[0] & 0x80) >>> 0 == 0x80) {
+        ret = (ret | (0xff << 24)) >>> 0;
     }
 
     return ret;
